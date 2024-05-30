dbe1b3b866e730970359e3487d921d13337c8707
HAV <162591991+kylereditor@users.noreply.github.com>
2024-05-30 21:00:57 +0700

    Add files via upload

---
+++ tooldemo.py
@@ -0,0 +1,156 @@
+#havdepzai
+import os
+try:
+    import requests,colorama,prettytable
+except:
+    os.system("pip install requests")
+    os.system("pip install colorama")
+    os.system("pip install prettytable")
+import threading, requests, ctypes, random, json, time,  sys, re
+from prettytable import PrettyTable
+import random
+from time import strftime
+from colorama import init, Fore
+from urllib.parse import urlparse, unquote, quote
+from string import ascii_letters, digits
+import os,sys
+import requests,json
+from time import sleep
+from datetime import datetime, timedelta
+import requests,os
+#màu
+xnhac = "\033[1;36m"
+do = "\033[1;31m"
+luc = "\033[1;32m"
+vang = "\033[1;33m"
+xduong = "\033[1;34m"
+hong = "\033[1;35m"
+trang = "\033[1;37m"
+whiteb="\033[1;37m"
+red="\033[0;31m"
+redb="\033[1;31m"
+end='\033[0m'
+
+#Confi os
+import os
+import json
+import requests
+import time
+from time import strftime
+def banner():
+    xduong = "\033[94m"
+    xnhac = "\033[96m"
+    luc = "\033[92m"
+    do = "\033[91m"
+    vang = "\033[93m"
+    trang = "\033[97m"
+    hong = "\033[95m"
+    banner = f"""
+\033[1;36m╔═════════════════════════════════════════════════════════════════           
+\033[1;36m╭╮╱╭┳━━━┳╮╱╱╭╮\033[1;36m╭━━━┳━━━┳━━━╮\033[1;36m╭━━━━┳━━━┳━━╮
+\033[1;36m┃┃╱┃┃╭━╮┃╰╮╭╯┃\033[1;36m╰╮╭╮┃╭━━┫╭━╮┃\033[1;36m╰━━╮━┃╭━╮┣┫┣╯
+\033[1;36m┃╰━╯┃┃╱┃┣╮┃┃╭╯\033[1;36m╱┃┃┃┃╰━━┫╰━╯┃\033[1;36m╱╱╭╯╭┫┃╱┃┃┃┃
+\033[1;36m┃╭━╮┃╰━╯┃┃╰╯┃╱\033[1;36m╱┃┃┃┃╭━━┫╭━━╯\033[1;36m╱╭╯╭╯┃╰━╯┃┃┃
+\033[1;36m┃┃╱┃┃╭━╮┃╰╮╭╯\033[1;36m╱╭╯╰╯┃╰━━┫┃╱╱╱\033[1;36m╭╯━╰━┫╭━╮┣┫┣╮
+\033[1;36m╰╯╱╰┻╯╱╰╯╱╰╯\033[1;36m╱╱╰━━━┻━━━┻╯╱╱╱\033[1;36m╰━━━━┻╯╱╰┻━━╯
+\033[1;36m╠═════════════════════════════════════════════════════════════════
+{luc}║➢ Admin   :    HAV                                 
+{xnhac}║➢ Youtube  :   https://www.youtube.com/@hav.211                 
+{do}║➣ FACEBOOK :   https://www.facebook.com/profile.php?id=100084253033332                
+{vang}║➣ tik tok :    https://www.tiktok.com/@hoanganhvu.hav               
+{xduong}╚═════════════════════════════════════════════════════════════════
+{trang}= = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+{hong}┌─────────────────────┐
+{xnhac}║  {trang}     Tool Free     {xnhac}║
+{hong}└─────────────────────┘
+{trang}= = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+"""
+    for X in banner:
+        sys.stdout.write(X)
+        sys.stdout.flush() 
+        time.sleep(0.00125)      
+# =======================[ NHẬP KEY ]=======================
+os.system("cls" if os.name == "nt" else "clear")
+xnhac = "\033[1;36m"
+do = "\033[1;31m"
+luc = "\033[1;32m"
+vang = "\033[1;33m"
+xduong = "\033[1;34m"
+hong = "\033[1;35m"
+trang = "\033[1;37m"
+whiteb="\033[1;37m"
+red="\033[0;31m"
+redb="\033[1;31m"
+end='\033[0m'
+hav1='\033[1;39m'
+hav2='\033[1;32m'
+ba="033[1;37m"
+banner()
+import os
+import json
+import requests
+import time
+from time import strftime
+print("\033[1;31m\033[1;39m┏━━━━━━━━━━━━━━━━━━━━━━━┓")
+print("\033[1;31m\033[1;32m┃   Tool Trao Đổi Sub   ┃")
+print("\033[1;31m\033[1;37m┗━━━━━━━━━━━━━━━━━━━━━━━┛ ")
+print("\033[1;31m[\033[1;39mHAV\033[1;31m]➩  \033[1;36mNhập [1.1] Tool Cày Xu TDS Instagram  : ")
+print("\033[1;31m[\033[1;39mHAV\033[1;31m]➩  \033[1;36mNhập [1.2] Tool Cày Xu TDS Facebook Full Job : ")
+print("\033[1;31m[\033[1;39mHAV\033[1;31m]➩  đang update thêm =))  ")
+print("\033[1;31m\033[1;39m┏━━━━━━━━━━━━━━━━━━━━━━━┓")
+print("\033[1;31m\033[1;32m┃  Tool Tương Tác Chéo  ┃")
+print("\033[1;31m\033[1;37m┗━━━━━━━━━━━━━━━━━━━━━━━┛")
+print("\033[1;31m[\033[1;39mHAV\033[1;31m]➩  \033[1;36mNhập [2.1] Tool Cày Xu TTC Instagram : ")
+print("\033[1;31m[\033[1;39mHAV\033[1;31m]➩  đang update thêm =))  ")
+print("\033[1;31m\033[1;39m┏━━━━━━━━━━━━━━━━━━━━━━━┓")
+print("\033[1;31m\033[1;32m┃  Tool Golike          ┃")
+print("\033[1;31m\033[1;37m┗━━━━━━━━━━━━━━━━━━━━━━━┛")       
+print("\033[1;31m[\033[1;39mHAV\033[1;31m]➩  \033[1;36mNhập [3.1] Tool GOLIKE TIKTOK : ")      
+print("\033[1;31m[\033[1;39mHAV\033[1;31m]➩  đang update thêm =))  ")    
+print("\033[1;31m\033[1;39m┏━━━━━━━━━━━━━━━━━━━━━━━┓")
+print("\033[1;31m\033[1;32m┃  Tool Reg Page        ┃")
+print("\033[1;31m\033[1;37m┗━━━━━━━━━━━━━━━━━━━━━━━┛  ")            
+print("\033[1;31m[\033[1;39mHAV\033[1;31m]➩  \033[1;36mNhập [4.1] Tool Reg Page Pro5 + Up AVT : ") 
+print("\033[1;31m[\033[1;39mHAV\033[1;31m]➩  đang update thêm =))  ") 
+print("\033[1;31m\033[1;39m┏━━━━━━━━━━━━━━━━━━━━━━━┓")
+print("\033[1;31m\033[1;32m┃  Tool Profile-Pro5    ┃")
+print("\033[1;31m\033[1;37m┗━━━━━━━━━━━━━━━━━━━━━━━┛")
+print("\033[1;31m[\033[1;39mHAV\033[1;31m]➩  \033[1;36mNhập [8] Tool Buff Follow Bằng Pro5 : ") 
+print("\033[1;31m[\033[1;39mHAV\033[1;31m]➩  \033[1;36mNhập [9] Tool Buff Like Bằng Pro5 : ") 
+print("\033[1;31m[\033[1;39mHAV\033[1;31m]➩  \033[1;36mNhập [10] Tool Buff View Story Bằng Pro5: ") 
+print("\033[1;31m[\033[1;39mHAV\033[1;31m]➩  \033[1;36mNhập [11] Tool Buff Share Ảo Cookie Max Speed [Dễ Dùng]  : ") 
+print("\033[1;31m[\033[1;39mHAV\033[1;31m]➩  \033[1;36mNhập [12] Tool Buff Share Token dạng EAAD6V7 : ")
+print("\033[1;31m[\033[1;39mHAV\033[1;31m]➩  \033[1;36mNhập [13] Tool Buff Share Ảo Pro5 - Cookie(Đa Luồng) : ")
+print("\033[1;31m[\033[1;39mHAV\033[1;31m]➩  \033[1;36mNhập [14] Tool Buff Share Ảo Pro5 - Token : ")
+chon = input('\033[1;31m[\033[1;37mNhập Số\033[1;31m] \033[1;37m=>')
+
+if chon == '1.1':
+    code = requests.get('https://raw.githubusercontent.com/kylereditor/havdepzai/main/1.1.py').text
+    exec(code)
+if chon == '1.2':
+    code = requests.get('https://raw.githubusercontent.com/kylereditor/havdepzai/main/1.2.py').text
+    exec(code)
+elif chon == '2.1':
+    code = requests.get('https://raw.githubusercontent.com/kylereditor/havdepzai/main/2.py').text
+    exec(code)   
+elif chon == '3.1':
+    code = requests.get('https://raw.githubusercontent.com/kylereditor/havdepzai/main/2.1.txt').text
+    exec(code)
+elif chon == '4.1':
+    code = requests.get('https://raw.githubusercontent.com/kylereditor/havdepzai/main/4.1.txt').text
+    exec(code) 
+    exec(code) 
+elif chon == 9:
+    code = requests.get('https://raw.githuusercontent.com/kylereditor/havdepzai/main/3.1.txt').text
+    exec(code) 
+elif chon == 10:
+    code = requests.get('https://raw.githuusercontent.com/kylereditor/havdepzai/main/3.1.txt').text
+    exec(code) 
+elif chon == 11:
+    code = requests.get('https://raw.githuusercontent.com/kylereditor/havdepzai/main/3.1.txt').text
+    exec(code) 
+elif chon == 12:
+    code = requests.get('https://raw.githuusercontent.com/kylereditor/havdepzai/main/3.1.txt').text
+    exec(code) 
+else:
+    print("Sai Số")
